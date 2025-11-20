17d59e9eb64daa6c31cc84b1bba94a37e596f7e5
Fenst <rkrb@tyfonmail.se>
2025-11-20 23:17:06 +0100

    -refactored routing into nested routing from separate routing -added a header that links to /

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -3,36 +3,34 @@ import App from "../App.tsx";
 import {LandingPage} from "../pages/LandingPage/LandingPage.tsx";
 import {CategoryPage} from "@/pages/CategoryPage/CategoryPage.tsx";
 import {RecipePage} from "@/pages/RecipePage/RecipePage.tsx";
+import {Header} from "@/components/Header.tsx";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <App/>,
+        element: (
+            <>
+                <Header/>
+                <App/>
+            </>
+        ),
         children: [
             {
                 index: true,
                 element: <LandingPage/>
-            }
-        ],
-    },
-    {
-        path: "category/:category",
-        element: <App/>,
-        children: [
+
+            },
             {
+                path: "category/:category",
                 index: true,
                 element: <CategoryPage/>
-            }
-        ],
-    },
-    {
-        path: "recipe/:recipeId",
-        element: <App/>,
-        children: [
+            },
             {
+                path: "recipe/:recipeId",
                 index: true,
                 element: <RecipePage/>
             }
         ],
-    }
+    },
+
 ]);
